9cdc5324b5065ce4ab0eba65617e8bdd285d2e87
Catherine Kim <catherine@ipwe.com>
2020-07-12 16:43:04 -0700

    Add 13_file_io.py Because: - Solved 2 problems.

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,20 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+# f = open('foo.txt', 'r')
+# print(f.read())
+# f.close()
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
-# writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make
-# sure that it contains what you expect it to contain
+# writing. Write three lines of arbitrary content to that file, then close the file. 
+# Open up "bar.txt" and inspect it to make sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+b = open('bar.txt', 'w')
+b.write('Let`s keep this love\n')
+b.write('yay yay yay yay\n')
+b.write('love, love love love')
+b.close()
+bf = open('bar.txt', 'r')
+print(bf.read())
